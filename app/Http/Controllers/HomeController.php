69d8faa143755ba61250845c3eafb49053fcc37e
ONeal <kigat63@gmail.com>
2020-11-11 20:32:35 +0300

    Laravel learn Controller part 1

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class HomeController extends Controller
+{
+
+
+    public function index(){
+        return view('home', ['res'=>5,'name'=>'jojo']);
+    }
+
+    public function test(){
+        return __METHOD__;
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/test', [HomeController::class, 'test']);
+Route::get('/page/{slug}', [PageController::class, 'show']);
 // Route::get('/', function () {
 //     return view('welcome');
 // });
@@ -33,15 +38,15 @@ use Illuminate\Support\Facades\Route;
 //     return view('home', ['var'=>$res, 'name'=>$name]);
 // });
 
-Route::get('/', function () {
-    $res = 2 + 3;
-    $name = 'jojo';
-    return view('home', compact('res','name'));
-});
+// Route::get('/', function () {
+//     $res = 2 + 3;
+//     $name = 'jojo';
+//     return view('home', compact('res','name'));
+// });
 
-Route::get('/about', function () {
-    return 'This page is about';
-});
+// Route::get('/about', function () {
+//     return 'This page is about';
+// });
 
 // Route::get('/contact', function () {
 //     return view('contact');
@@ -54,41 +59,47 @@ Route::get('/about', function () {
 //     return 'send-email';
 // });
 
-Route::match(['get', 'post', 'put'], '/contact', function () {
-        if(!empty($_POST)){
-        dump($_POST);
-    }
-    return view('contact');
-})->name('contact');
+// Route::match(['get', 'post', 'put'], '/contact', function () {
+//         if(!empty($_POST)){
+//         dump($_POST);
+//     }
+//     return view('contact');
+// })->name('contact');
 
-Route::any('/any', function () {
-    if(!empty($_POST)){
-        dump($_POST);
-    }
-    return view('contact');
-});
+// Route::any('/any', function () {
+//     if(!empty($_POST)){
+//         dump($_POST);
+//     }
+//     return view('contact');
+// });
 
-Route::view('/test', 'test',['test_date'=>'test']);
+// Route::view('/test', 'test',['test_date'=>'test']);
 
-Route::redirect('/about', '/contact', 302);
+// Route::redirect('/about', '/contact', 302);
 
 // Route::get('/post/{id}/{slug}', function ($id,$slug) {
 //     return 'Post '.$id . ' slug ' . $slug;
 // })->where(['id'=> '[0-9]+','slug' => '[A-Za-z0-9-]+']);
 
-Route::get('/post/{id}/{slug?}', function ($id,$slug = null) {
-    return 'Post '.$id . ' slug ' . $slug;
-})->name('post');
-Route::prefix('admin')->name('admin.')->group(function () {
-    Route::get('/post', function () {
-        return 'posts list';
-    });
+// Route::get('/post/{id}/{slug?}', function ($id,$slug = null) {
+//     return 'Post '.$id . ' slug ' . $slug;
+// })->name('post');
+// Route::prefix('admin')->name('admin.')->group(function () {
+//     Route::get('/post', function () {
+//         return 'posts list';
+//     });
     
-    Route::get('/post/create', function () {
-        return 'posts create';
-    });
+//     Route::get('/post/create', function () {
+//         return 'posts create';
+//     });
     
-    Route::get('/post/{id}/edit', function ($id) {
-        return 'posts update ' .$id;
-    })->name('post');
-});
+//     Route::get('/post/{id}/edit', function ($id) {
+//         return 'posts update ' .$id;
+//     })->name('post');
+// });
+
+
+
+
+
+
