c7253739e5e6c4f3f6bb44716ab24d8c97658c0e
ONeal <kigat63@gmail.com>
2020-11-14 20:49:45 +0300

    Laravel leesons sql request

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+
 class HomeController extends Controller
 {
 
 
     public function index(){
+        // $query = DB::insert('INSERT into posts (title, text) VALUES(?,?) ', ['blog 4', 'Text from blog 4']);
+        // var_dump($query);
+        // DB::update('UPDATE posts SET title = ?, text = ? WHERE id = 9',['BLOG 2', 'Text from blog 2']);
+       $posts = DB::select("SELECT * FROM posts WHERE  id > :id",['id'=> 8]);
+       dump($posts);
         return view('home', ['res'=>5,'name'=>'jojo']);
     }
 
